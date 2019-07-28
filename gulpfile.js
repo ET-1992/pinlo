bf712493564112c48e871c63337a8304882d45e9
Die <die@97866.com>
2019-07-28 16:35:37 +0800

    init

--- gulpfile.js
+++ gulpfile.js
@@ -4,39 +4,78 @@ var gulp = require('gulp'),
   gulpAutoprefixer = require('gulp-autoprefixer'),
   extender = require('gulp-html-extend');
  
-gulp.task('connect', function() {
+gulp.task('connect-web', function() {
   connect.server({
-    root: 'web',
-    livereload: true
+    root: './src/web',
+    livereload: true,
+    port: 8081
   });
 });
  
-gulp.task('html', function () {
-  gulp.src('./web/*.html')
+gulp.task('html-web', function () {
+  gulp.src('./src/web/*.html')
     .pipe(extender({annotations:true,verbose:false}))
-    .pipe(gulp.dest('./web/dist/html'))
+    .pipe(gulp.dest('./src/web/dist/html'))
     .pipe(connect.reload());
 });
 
-gulp.task('sass', function () {
-    gulp.src('./web/scss/*.scss')
+gulp.task('sass-web', function () {
+    gulp.src('./src/web/scss/*.scss')
       .pipe(gulpSass())
       .pipe(gulpAutoprefixer())
-      .pipe(gulp.dest('./web/dist'))
+      .pipe(gulp.dest('./src/web/dist'))
       .pipe(connect.reload())
   });
 
-gulp.task('image', function () {
-  gulp.src('./web/image/*')
-    .pipe(gulp.dest('./web/dist/image'))
+gulp.task('image-web', function () {
+  gulp.src('./src/web/image/*')
+    .pipe(gulp.dest('./src/web/dist/image'))
     .pipe(connect.reload())
 });
 
 
-gulp.task('watch', function () {
-  gulp.watch(['./web/*.html'], ['html']);
-  gulp.watch(['./web/scss/*.scss'], ['sass']);
-  gulp.watch(['./web/image/*'], ['image']);
+gulp.task('watch-web', function () {
+  gulp.watch(['./src/web/*.html'], ['html-web']);
+  gulp.watch(['./src/web/scss/*.scss'], ['sass-web']);
+  gulp.watch(['./src/web/image/*'], ['image-web']);
 });
  
-gulp.task('default', ['connect', 'html', 'sass', 'image','watch']);
+gulp.task('web', ['connect-web', 'html-web', 'sass-web', 'image-web','watch-web']);
+
+gulp.task('connect-h5', function() {
+  connect.server({
+    root: './src/h5',
+    livereload: true,
+    port: 8080
+  });
+});
+ 
+gulp.task('html-h5', function () {
+  gulp.src('./src/h5/*.html')
+    .pipe(extender({annotations:true,verbose:false}))
+    .pipe(gulp.dest('./src/h5/dist/html'))
+    .pipe(connect.reload());
+});
+
+gulp.task('sass-h5', function () {
+    gulp.src('./src/h5/scss/*.scss')
+      .pipe(gulpSass())
+      .pipe(gulpAutoprefixer())
+      .pipe(gulp.dest('./src/h5/dist'))
+      .pipe(connect.reload())
+  });
+
+gulp.task('image-h5', function () {
+  gulp.src('./src/h5/image/*')
+    .pipe(gulp.dest('./src/h5/dist/image'))
+    .pipe(connect.reload())
+});
+
+
+gulp.task('watch-h5', function () {
+  gulp.watch(['./src/h5/*.html'], ['html-h5']);
+  gulp.watch(['./src/h5/scss/*.scss'], ['sass-h5']);
+  gulp.watch(['./src/h5/image/*'], ['image-h5']);
+});
+ 
+gulp.task('h5', ['connect-h5', 'html-h5', 'sass-h5', 'image-h5','watch-h5']);
